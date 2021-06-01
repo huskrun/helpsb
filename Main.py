89d79af3f03f573ace911b6a7cca481d95ff361f
Xlioxide <80074222+Xlioxide@users.noreply.github.com>
2021-06-01 09:02:14 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 class ServerNuker():
-    version = 3.5
+    __version__ = 3.5
 
 import discord, subprocess, sys, time, os, colorama, base64, codecs, datetime, io, random, numpy, datetime, smtplib, string, ctypes, pokepy
 import urllib.parse, urllib.request, re, json, requests, webbrowser, aiohttp, dns.name, asyncio, functools, logging
@@ -23,7 +23,7 @@ from gtts import gTTS
 from randomuser import RandomUser
 from pythonping import ping as pyping
 
-ctypes.windll.kernel32.SetConsoleTitleW(f'[Flash Server Wizz Tool v{ServerNuker.version}] | Loading in...')
+ctypes.windll.kernel32.SetConsoleTitleW(f'[Flash Server Wizz Tool v{ServerNuker.__version__}] | Loading in...')
 
 with open('config.json') as f:
     config = json.load(f)
@@ -33,16 +33,16 @@ password = config.get('pass')
 prefix = config.get('prefix')
 anti_afk = config.get('anti_afk')
 
-width = os.get_terminal_size().columns
+width = os.get_terminal_size().columns  
 def startprint():
      if anti_afk == True:
          antiafk = 'Enabled'
      else:
         antiafk = 'Disabled'
-        
-print(f'''{Fore.RESET}
-                     {Fore.YELLOW}▄████  ▄▄▄       ███▄    █   ▄████   ██████ ▄▄▄█████▓ █    ██  ██░ ██  ██░ ██ 
-                     {Fore.YELLOW}██▒ ▀█▒▒████▄     ██ ▀█   █  ██▒ ▀█▒▒██    ▒ ▓  ██▒ ▓▒ ██  ▓██▒▓██░ ██▒▓██░ ██▒
+
+     print(f'''{Fore.RESET}
+                     {Fore.YELLOW}  ▄████  ▄▄▄       ███▄    █   ▄████   ██████ ▄▄▄█████▓ █    ██  ██░ ██  ██░ ██ 
+                     {Fore.YELLOW} ██▒ ▀█▒▒████▄     ██ ▀█   █  ██▒ ▀█▒▒██    ▒ ▓  ██▒ ▓▒ ██  ▓██▒▓██░ ██▒▓██░ ██▒
                      {Fore.YELLOW}▒██░▄▄▄░▒██  ▀█▄  ▓██  ▀█ ██▒▒██░▄▄▄░░ ▓██▄   ▒ ▓██░ ▒░▓██  ▒██░▒██▀▀██░▒██▀▀██░
                      {Fore.YELLOW}░▓█  ██▓░██▄▄▄▄██ ▓██▒  ▐▌██▒░▓█  ██▓  ▒   ██▒░ ▓██▓ ░ ▓▓█  ░██░░▓█ ░██ ░▓█ ░██ 
                      {Fore.YELLOW}░▒▓███▀▒ ▓█   ▓██▒▒██░   ▓██░░▒▓███▀▒▒██████▒▒  ▒██▒ ░ ▒▒█████▓ ░▓█▒░██▓░▓█▒░██▓
@@ -50,12 +50,15 @@ print(f'''{Fore.RESET}
                      {Fore.YELLOW}  ░   ░   ▒   ▒▒ ░░ ░░   ░ ▒░  ░   ░ ░ ░▒  ░ ░    ░    ░░▒░ ░ ░  ▒ ░▒░ ░ ▒ ░▒░ ░
                      {Fore.YELLOW}░ ░   ░   ░   ▒      ░   ░ ░ ░ ░   ░ ░  ░  ░    ░       ░░░ ░ ░  ░  ░░ ░ ░  ░░ ░
                      {Fore.YELLOW}      ░       ░  ░         ░       ░       ░              ░      ░  ░  ░ ░  ░  ░
-
+                                                                                
+                                    
                         {Fore.RED}Logged In As ==> {Fore.WHITE}{Ioxide.user.name}#{Ioxide.user.discriminator}{Fore.WHITE}
                         {Fore.RED}ID ==> {Fore.WHITE}{Ioxide.user.id}
                         {Fore.RED}Anti-AFK ==> {Fore.WHITE}{antiafk}
-                        {Fore.RED}Version ==> {Fore.WHITE} v{ServerNuker.version}
+                        {Fore.RED}Version ==> {Fore.WHITE} v{ServerNuker.__version__}
                     '''+Fore.RESET)
+
+
 def Clear():
     os.system('cls')
 Clear()
@@ -92,3 +95,80 @@ Ioxide = commands.Bot(
     command_prefix=prefix,
     self_bot=True
 )
+Ioxide.remove_command('help') 
+
+@Ioxide.event
+async def on_command_error(ctx, error):
+    error_str = str(error)
+    error = getattr(error, 'original', error)
+    if isinstance(error, commands.CommandNotFound):
+        return
+    elif isinstance(error, commands.CheckFailure):
+        print(f"{Fore.WHITE}Error: {Fore.YELLOW}You're missing permission to execute this command"+Fore.RESET)
+    elif isinstance(error, commands.MissingRequiredArgument):
+        print(f"{Fore.WHITE}Error: {Fore.YELLOW}Missing arguments: {error}"+Fore.RESET)
+    elif isinstance(error, numpy.AxisError):
+        print(f"{Fore.WHITE}Error: {Fore.YELLOW}Not a valid image"+Fore.RESET)
+    elif isinstance(error, discord.errors.Forbidden):
+        print(f"{Fore.WHITE}Error: {Fore.YELLOW}Discord error: {error}"+Fore.RESET)
+    elif "Cannot send an empty message" in error_str:
+        print(f"{Fore.WHITE}Error: {Fore.YELLOW}Couldnt send a empty message"+Fore.RESET)               
+    else:
+        print(f"{Fore.WHITE}Error: {Fore.YELLOW}{error_str}"+Fore.RESET)
+
+@Ioxide.event
+async def on_message_edit(before, after):
+    await Ioxide.process_commands(after)
+
+@Ioxide.event
+async def on_message(message):
+    responses = ["what nigga", "shut up and focus son", "nigga im right here lmfao", "sybau and focus on gettin blazed","I got 2 dicks in my ass and u still cant pack me. fuck up nigga"]
+    nums = [1, 1.3, 1.5, 2, 2.3, 2.6, 3, 3.8, 5, 4, 6, 7]
+    ptime = random.choice(nums)
+    ## Begin Long Annoying Anti-AFK Checking..
+    if 'afk c' in message.content:
+        if anti_afk == True:
+                try:
+                    randomResponses = random.choice(responses)
+                    msg = randomResponses
+                    time.sleep(ptime)
+                    await message.channel.send(msg)
+                except discord.errors.Forbidden:
+                    print(""
+                    f"\n{Fore.RED}was unable to send message at{Fore.WHITE} {time}"+Fore.RESET)
+    elif 'AFK C' in message.content:
+        if anti_afk == True:
+            try:
+                randomResponses = random.choice(responses)
+                time.sleep(ptime)
+                msg = randomResponses
+                await message.channel.send(msg)
+            except discord.errors.Forbidden:
+                print(""
+                f"\n{Fore.RED}was unable to send message at{Fore.WHITE} {time}"+Fore.RESET)
+
+    elif 'AFK c' in message.content:
+        if anti_afk == True:
+            try:
+                randomResponses = random.choice(responses)
+                time.sleep(ptime)
+                msg = randomResponses
+                await message.channel.send(msg)
+            except discord.errors.Forbidden:
+                print(""
+                f"\n{Fore.RED}was unable to send message at{Fore.WHITE} {time}"+Fore.RESET)
+        return
+
+    await Ioxide.process_commands(message)
+
+@Ioxide.event
+async def on_connect():
+    Clear()
+
+    if anti_afk == True:
+        antiafk = "Enabled"
+    else:
+        antiafk = "Disabled"
+
+    startprint()
+    ctypes.windll.kernel32.SetConsoleTitleW(f'[ Flash Nuker Tool v{ServerNuker.__version__} ] | Logged in as {Ioxide.user.name}')

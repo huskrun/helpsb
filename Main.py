81851786b2ffaf66f83b2009af3d495715a972b9
Xlioxide <80074222+Xlioxide@users.noreply.github.com>
2021-06-01 08:52:23 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 class ServerNuker():
-    __version__ = 1
+    version = 3.5
 
 import discord, subprocess, sys, time, os, colorama, base64, codecs, datetime, io, random, numpy, datetime, smtplib, string, ctypes, pokepy
 import urllib.parse, urllib.request, re, json, requests, webbrowser, aiohttp, dns.name, asyncio, functools, logging
@@ -23,7 +23,7 @@ from gtts import gTTS
 from randomuser import RandomUser
 from pythonping import ping as pyping
 
-ctypes.windll.kernel32.SetConsoleTitleW(f'[Husk Nuker v{ServerNuker.__version__}] | Loading in...')
+ctypes.windll.kernel32.SetConsoleTitleW(f'[Flash Server Wizz Tool v{ServerNuker.version}] | Loading in...')
 
 with open('config.json') as f:
     config = json.load(f)
@@ -31,12 +31,16 @@ with open('config.json') as f:
 token = config.get('token')
 password = config.get('pass')
 prefix = config.get('prefix')
+anti_afk = config.get('anti_afk')
 
-width = os.get_terminal_size().columns  
+width = os.get_terminal_size().columns
 def startprint():
-    
-    
-    print(f'''{Fore.RESET}
+     if anti_afk == True:
+         antiafk = 'Enabled'
+     else:
+        antiafk = 'Disabled'
+        
+print(f'''{Fore.RESET}
                      {Fore.YELLOW}▄████  ▄▄▄       ███▄    █   ▄████   ██████ ▄▄▄█████▓ █    ██  ██░ ██  ██░ ██ 
                      {Fore.YELLOW}██▒ ▀█▒▒████▄     ██ ▀█   █  ██▒ ▀█▒▒██    ▒ ▓  ██▒ ▓▒ ██  ▓██▒▓██░ ██▒▓██░ ██▒
                      {Fore.YELLOW}▒██░▄▄▄░▒██  ▀█▄  ▓██  ▀█ ██▒▒██░▄▄▄░░ ▓██▄   ▒ ▓██░ ▒░▓██  ▒██░▒██▀▀██░▒██▀▀██░
@@ -46,15 +50,12 @@ def startprint():
                      {Fore.YELLOW}  ░   ░   ▒   ▒▒ ░░ ░░   ░ ▒░  ░   ░ ░ ░▒  ░ ░    ░    ░░▒░ ░ ░  ▒ ░▒░ ░ ▒ ░▒░ ░
                      {Fore.YELLOW}░ ░   ░   ░   ▒      ░   ░ ░ ░ ░   ░ ░  ░  ░    ░       ░░░ ░ ░  ░  ░░ ░ ░  ░░ ░
                      {Fore.YELLOW}      ░       ░  ░         ░       ░       ░              ░      ░  ░  ░ ░  ░  ░
-                                
+
                         {Fore.RED}Logged In As ==> {Fore.WHITE}{Ioxide.user.name}#{Ioxide.user.discriminator}{Fore.WHITE}
                         {Fore.RED}ID ==> {Fore.WHITE}{Ioxide.user.id}
                         {Fore.RED}Anti-AFK ==> {Fore.WHITE}{antiafk}
-                        {Fore.RED}Version ==> {Fore.WHITE} v{ServerNuker.__version__}
+                        {Fore.RED}Version ==> {Fore.WHITE} v{ServerNuker.version}
                     '''+Fore.RESET)
-    
-
-
 def Clear():
     os.system('cls')
 Clear()
@@ -67,7 +68,7 @@ def Init():
         token = config.get('token')
         try:
             Ioxide.run(token, bot=False, reconnect=True)
-            os.system(f'title [ Husk SelfBot ] - Version {ServerNuker.__version__}')
+            os.system(f'title [ Flash Nuker ] - Version {ServerNuker.__version__}')
         except discord.errors.LoginFailure:
             print(f"{Fore.WHITE}[ERROR] {Fore.YELLOW}Sure this is a token? lol"+Fore.RESET)
             os.system('pause >NUL')
@@ -87,37 +88,7 @@ class Login(discord.Client):
 colorama.init()
 Ioxide = discord.Client()
 Ioxide = commands.Bot(
-    description='Husk Bot',
+    description='Flash Bot',
     command_prefix=prefix,
     self_bot=True
 )
-
-Ioxide.remove_command('help') 
-
-@Ioxide.event
-async def on_command_error(ctx, error):
-    error_str = str(error)
-    error = getattr(error, 'original', error)
-    if isinstance(error, commands.CommandNotFound):
-        return
-    elif isinstance(error, commands.CheckFailure):
-        print(f"{Fore.WHITE}Error: {Fore.YELLOW}You're missing permission to execute this command"+Fore.RESET)
-    elif isinstance(error, commands.MissingRequiredArgument):
-        print(f"{Fore.WHITE}Error: {Fore.YELLOW}Missing arguments: {error}"+Fore.RESET)
-    elif isinstance(error, numpy.AxisError):
-        print(f"{Fore.WHITE}Error: {Fore.YELLOW}Not a valid image"+Fore.RESET)
-    elif isinstance(error, discord.errors.Forbidden):
-        print(f"{Fore.WHITE}Error: {Fore.YELLOW}Discord error: {error}"+Fore.RESET)
-    elif "Cannot send an empty message" in error_str:
-        print(f"{Fore.WHITE}Error: {Fore.YELLOW}Couldnt send a empty message"+Fore.RESET)               
-    else:
-        print(f"{Fore.WHITE}Error: {Fore.YELLOW}{error_str}"+Fore.RESET)
-
-@Ioxide.event
-async def on_message_edit(before, after):
-    await Ioxide.process_commands(after)
-
-                   
-        
-
- 
